77f2178ad3b37d4a5173409f289d6323c6d1e4f7
heather <>
2002-04-22 17:25:36 +0000

    add mainpage

---
+++ src/mainpage.h
@@ -0,0 +1,17 @@
+/** @mainpage package RootIo
+  @author Heather Kelly
+
+  @section intro Introduction
+  This package defines the Gaudi algorithms that read and write ROOT files.
+  The following Gaudi algorithms are defined in this package:
+  - mcRootWriterAlg - writes a Monte Carloe ROOT file using the MC data on the TDS
+  - mcRootReaderAlg - read a Monte Carlo ROOT file, and puts MC data on the TDS
+
+  <hr>
+  @section notes release notes
+  release.notes
+  @section requirements requirements
+  @include requirements
+
+*/
+
