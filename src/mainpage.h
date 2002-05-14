0750f51c5eb334fa0553b60ea6acf4878d17be38
heather <>
2002-05-14 15:58:42 +0000

    update doc

--- src/mainpage.h
+++ src/mainpage.h
@@ -11,6 +11,13 @@
     -# compressionLevel - default (1) a value 0-9 where 9 is maximum compression
   - mcRootReaderAlg: read a Monte Carlo ROOT file, and puts MC data on the TDS
     -# mcRootFile - name of the input ROOT file
+  - digiRootWriterAlg:  writes a Digi ROOT file using digi data from the TDS.
+    -# digiRootFile - name of the output Digi ROOT file
+    -# splitMode - default (1)
+    -# bufferSize - default (64000)
+    -# compressionLevel - default (1) a value [0,9] where 9 is maximum compression
+  - digiRootReaderAlg:  reads in a Digi ROOT file and puts Digi data on the TDS
+    -# digiRootFile - name of in the input Digi ROOT file
 
   <hr>
   @section notes release notes
