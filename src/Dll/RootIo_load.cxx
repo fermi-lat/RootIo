c18061eeb53e2aed7e270768a9da8b35ef6b6ac1
heather <>
2002-05-15 22:35:26 +0000

    adding new recon ROOT algs

--- src/Dll/RootIo_load.cxx
+++ src/Dll/RootIo_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/Dll/RootIo_load.cxx,v 1.1.1.1 2002/04/22 17:10:24 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/Dll/RootIo_load.cxx,v 1.2 2002/05/14 15:19:00 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -13,5 +13,8 @@ DECLARE_FACTORY_ENTRIES(RootIo) {
     DECLARE_ALGORITHM( mcRootReaderAlg );
     DECLARE_ALGORITHM( digiRootWriterAlg );
     DECLARE_ALGORITHM( digiRootReaderAlg );
+    DECLARE_ALGORITHM( reconRootWriterAlg );
+    DECLARE_ALGORITHM( reconRootReaderAlg );
+
 }
   
---
+++ src/reconRootReaderAlg.cxx
@@ -0,0 +1,221 @@
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/IDataProviderSvc.h"
+#include "GaudiKernel/SmartDataPtr.h"
+#include "GaudiKernel/Algorithm.h"
+
+#include "Event/TopLevel/Event.h"
+#include "Event/TopLevel/EventModel.h"
+#include "idents/CalXtalId.h"
+
+#include "TROOT.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TDirectory.h"
+#include "TObjArray.h"
+#include "TCollection.h"  // Declares TIter
+
+#include "reconRootData/ReconEvent.h"
+
+#include "facilities/Util.h"
+
+
+/** @class reconRootReaderAlg
+ * @brief Reads Reconstruction data from a persistent ROOT file and stores the
+ * the data in the TDS.
+ *
+ * @author Heather Kelly
+ * $Header$
+ */
+
+class reconRootReaderAlg : public Algorithm
+{	
+public:
+    
+    reconRootReaderAlg(const std::string& name, ISvcLocator* pSvcLocator);
+    
+    /// Handles setup by opening ROOT file in read mode and creating a new TTree
+    StatusCode initialize();
+   
+    /// Orchastrates reading from ROOT file and storing the data on the TDS for each event
+    StatusCode execute();
+    
+    /// Closes the ROOT file and cleans up
+    StatusCode finalize();
+            
+private:
+
+    /// Reads top-level DigiEvent
+    StatusCode readReconEvent();
+
+    /// Reads TKR recon data from ROOT and puts data on TDS
+    StatusCode readTkrRecon();
+
+    /// Reads CAL recon data from ROOT and put data on TDS
+    StatusCode readCalRecon();
+
+    /// Closes the ROOT file
+    void close();
+   
+    /// ROOT file pointer
+    TFile *m_reconFile;
+    /// ROOT tree pointer
+    TTree *m_reconTree;
+    /// Top-level Monte Carlo ROOT object
+    ReconEvent *m_reconEvt;
+    /// name of the output ROOT file
+    std::string m_fileName;
+    /// name of the Recon TTree stored in the ROOT file
+    std::string m_treeName;
+
+};
+
+static const AlgFactory<reconRootReaderAlg>  Factory;
+const IAlgFactory& reconRootReaderAlgFactory = Factory;
+
+
+reconRootReaderAlg::reconRootReaderAlg(const std::string& name, ISvcLocator* pSvcLocator) : 
+Algorithm(name, pSvcLocator)
+{
+    // Input pararmeters that may be set via the jobOptions file
+    // Input ROOT file name
+    declareProperty("reconRootFile",m_fileName="recon.root");
+    // Input TTree name
+    declareProperty("reconTreeName", m_treeName="Recon");
+
+}
+
+StatusCode reconRootReaderAlg::initialize()
+{
+    // Purpose and Method:  Called once before the run begins.  This method
+    //    opens a new ROOT file and prepares for reading.
+
+    StatusCode sc = StatusCode::SUCCESS;
+    MsgStream log(msgSvc(), name());
+    
+    // Use the Job options service to set the Algorithm's parameters
+    // This will retrieve parameters set in the job options file
+    setProperties();
+
+    facilities::Util::expandEnvVar(&m_fileName);
+    
+    // Save the current directory for the ntuple writer service
+    TDirectory *saveDir = gDirectory;   
+    m_reconFile = new TFile(m_fileName.c_str(), "READ");
+    if (!m_reconFile->IsOpen()) sc = StatusCode::FAILURE;
+    m_reconFile->cd();
+    m_reconTree = (TTree*)m_reconFile->Get(m_treeName.c_str());
+    m_reconEvt = 0;
+    m_reconTree->SetBranchAddress("ReconEvent", &m_reconEvt);
+    
+    saveDir->cd();
+    return sc;
+    
+}
+
+StatusCode reconRootReaderAlg::execute()
+{
+    // Purpose and Method:  Called once per event.  This method calls
+    //   the appropriate methods to read data from the ROOT file and store
+    //   data on the TDS.
+
+    MsgStream log(msgSvc(), name());
+
+    StatusCode sc = StatusCode::SUCCESS;
+
+    static UInt_t evtId = 0;
+    m_reconTree->GetEvent(evtId);
+
+    sc = readReconEvent();
+    if (sc.isFailure()) {
+        log << MSG::ERROR << "Failed to read top level ReconEvent" << endreq;
+        return sc;
+    }
+
+    sc = readTkrRecon();
+    if (sc.isFailure()) {
+        log << MSG::ERROR << "Failed to load Tkr Recon" << endreq;
+        return sc;
+    }
+
+
+    sc = readCalRecon();
+    if (sc.isFailure()) {
+        log << MSG::ERROR << "Failed to load Cal Recon" << endreq;
+        return sc;
+    }
+
+    m_reconEvt->Clear();
+    evtId++;
+    
+    return sc;
+}
+
+
+StatusCode reconRootReaderAlg::readReconEvent() {
+
+    MsgStream log(msgSvc(), name());
+
+    StatusCode sc = StatusCode::SUCCESS;
+    
+    // Retrieve the Event data for this event
+    SmartDataPtr<Event::EventHeader> evt(eventSvc(), EventModel::EventHeader);
+    if (!evt) {
+        log << MSG::ERROR << "Failed to retrieve Event" << endreq;
+        return StatusCode::FAILURE;
+    }
+
+    unsigned int eventIdTds = evt->event();
+    unsigned int runIdTds = evt->run();
+
+    unsigned int eventIdRoot = m_reconEvt->getEventId();
+    unsigned int runIdRoot = m_reconEvt->getRunId();
+
+    // Check to see if the event and run ids have already been set.
+    if (eventIdTds != eventIdRoot) evt->setEvent(eventIdRoot);
+    if (runIdTds != runIdTds) evt->setRun(runIdRoot);
+
+    return sc;
+}
+
+StatusCode reconRootReaderAlg::readTkrRecon() {
+    MsgStream log(msgSvc(), name());
+
+    StatusCode sc = StatusCode::SUCCESS;
+
+ 
+    return sc;
+}
+
+
+
+StatusCode reconRootReaderAlg::readCalRecon() {
+    MsgStream log(msgSvc(), name());
+
+    StatusCode sc = StatusCode::SUCCESS;
+
+ 
+    return sc;
+}
+
+void reconRootReaderAlg::close() 
+{
+    // Purpose and Method:  Writes the ROOT file at the end of the run.
+    //    The TObject::kOverWrite parameter is used in the Write method
+    //    since ROOT will periodically write to the ROOT file when the bufSize
+    //    is filled.  Writing would create 2 copies of the same tree to be
+    //    stored in the ROOT file, if we did not specify kOverwrite.
+
+    TDirectory *saveDir = gDirectory;
+    m_reconFile->cd();
+    m_reconFile->Close();
+    saveDir->cd();
+}
+
+StatusCode reconRootReaderAlg::finalize()
+{
+    close();
+    
+    StatusCode sc = StatusCode::SUCCESS;
+    return sc;
+}
