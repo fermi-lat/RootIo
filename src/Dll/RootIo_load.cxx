c5347da6af7f0a0e7d190ee7649766cb8df626c4
heather <>
2003-06-02 01:53:19 +0000

    add RootIoSvc

--- src/Dll/RootIo_load.cxx
+++ src/Dll/RootIo_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/Dll/RootIo_load.cxx,v 1.4 2002/10/08 22:17:03 kyoung Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/Dll/RootIo_load.cxx,v 1.5 2002/12/02 21:50:09 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -18,6 +18,8 @@ DECLARE_FACTORY_ENTRIES(RootIo) {
     DECLARE_ALGORITHM( relationRootWriterAlg );
     DECLARE_ALGORITHM( relationRootReaderAlg );
     DECLARE_TOOL( RootIoRandom );
+    DECLARE_SERVICE( RootIoSvc );
+
 
 }
   
