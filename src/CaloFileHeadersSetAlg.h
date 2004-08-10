93fa3b7ea4d8d464195fb2b8dc8412f5a05d5b0f
chamont <>
2004-08-10 14:47:43 +0000

    some more class documentation

--- src/CaloFileHeadersSetAlg.h
+++ src/CaloFileHeadersSetAlg.h
@@ -1,12 +1,23 @@
 
 #include "GaudiKernel/Algorithm.h"
 
-/** @class CaloFileHeadersSetAlg
- * @brief Prepare the calo specific attributes and set them in the relevant headers.
- *
- * @author David Chamont
- * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/digiRootWriterAlg.cxx,v 1.24 2004/03/25 20:18:14 heather Exp $
- */
+/*!
+
+ @class CaloFileHeadersSetAlg        
+ @brief Prepare the calo specific attributes and set them in the relevant file headers
+
+ This Gaudi algorithm is expected to prepare and set the file header data which is
+ specific to calorimetry. It is a kind of demo class, to be reused as is and/or
+ eventually transformed by calo developers. It should be declared in the job options
+ top algorithms.
+ 
+ As any algorithm which is preparing file headers data, it is using the tool
+ FileHeadersTool so to get access to the current writable headers. Worth to note,
+ the task is done during the finalize() step.
+
+ @author David Chamont - CNRS IN2P3 LLR Ecole Polytechnique
+
+*/
 
 class CaloFileHeadersSetAlg : public Algorithm  {
         
--- src/FileHeadersSetAlg.h
+++ src/FileHeadersSetAlg.h
@@ -1,11 +1,30 @@
 
 #include "GaudiKernel/Algorithm.h"
 
+/*!
+
+ @class FileHeadersSetAlg        
+ @brief Prepare the common attributes and set them in all available headers
+
+ This Gaudi algorithm is expected to prepare and set the file header data which is
+ common to all files. Its implementation implicitly define what is the header
+ data common to all ROOT files. It should be declared in the job options top algorithms,
+ each time that a new ROOT file should be produced by the job.
+ 
+ As any algorithm which is preparing file headers data, it is using the tool
+ FileHeadersTool so to get access to the current writable headers. On the other
+ contrary of other algorithms, it is setting the same common data in all
+ the available writable headers (mc, digi and recon).
+ Worth to note, the task is done during the finalize() step.
+
+ @author David Chamont - CNRS IN2P3 LLR Ecole Polytechnique
+
+*/
 /** @class FileHeadersSetAlg
  * @brief Prepare the common attributes and set them in available headers.
  *
  * @author David Chamont
- * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/digiRootWriterAlg.cxx,v 1.24 2004/03/25 20:18:14 heather Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/FileHeadersSetAlg.h,v 1.1 2004/08/09 17:57:31 chamont Exp $
  */
 
 class FileHeadersSetAlg : public Algorithm {	
