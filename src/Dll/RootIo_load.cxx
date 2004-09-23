014fab66955c26b1a3cf2a8e948c16b6dbb4923e
heather <>
2004-09-23 17:55:11 +0000

    add testFileHeadersSetAlg

--- src/Dll/RootIo_load.cxx
+++ src/Dll/RootIo_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/Dll/RootIo_load.cxx,v 1.7 2004/08/09 17:57:31 chamont Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/Dll/RootIo_load.cxx,v 1.8 2004/08/27 13:55:40 chamont Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -11,6 +11,7 @@
 DECLARE_FACTORY_ENTRIES(RootIo) {
     DECLARE_ALGORITHM( FileHeadersSetAlg );
     DECLARE_ALGORITHM( testCaloFileHeadersSetAlg );
+	DECLARE_ALGORITHM( testFileHeadersSetAlg );
     DECLARE_ALGORITHM( mcRootWriterAlg );
     DECLARE_ALGORITHM( mcRootReaderAlg );
     DECLARE_ALGORITHM( digiRootWriterAlg );
