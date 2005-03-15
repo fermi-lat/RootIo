1aeca77d74558a4fd54c76c0f7822bb65b161837
heather <>
2005-03-15 23:28:00 +0000

    store all diag and err lengths

--- src/digiRootWriterAlg.cxx
+++ src/digiRootWriterAlg.cxx
@@ -43,7 +43,7 @@
  * @brief Writes Digi TDS data to a persistent ROOT file.
  *
  * @author Heather Kelly
- * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/digiRootWriterAlg.cxx,v 1.41 2005/02/22 21:06:28 heather Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/digiRootWriterAlg.cxx,v 1.42 2005/03/01 20:00:24 heather Exp $
  */
 
 class digiRootWriterAlg : public Algorithm
@@ -324,15 +324,15 @@ StatusCode digiRootWriterAlg::writeEventSummary() {
     }
     m_digiEvt->getEventSummaryData().initialize(summaryTds->summary());
     m_digiEvt->getEventSummaryData().initEventFlags(summaryTds->eventFlags());
-    const unsigned int nTem = 16;
-    unsigned int tem[nTem];
-    unsigned int iTem;
-    for (iTem = 0; iTem < nTem; iTem++) {
-        tem[iTem] = summaryTds->temLength(iTem);
-    }
-    m_digiEvt->getEventSummaryData().initContribLen(tem, 
+    //const unsigned int nTem = 16;
+    //unsigned int tem[nTem];
+    //unsigned int iTem;
+    //for (iTem = 0; iTem < nTem; iTem++) {
+    //    tem[iTem] = summaryTds->temLength(iTem);
+    //}
+    m_digiEvt->getEventSummaryData().initContribLen(summaryTds->temLength(), 
         summaryTds->gemLength(), summaryTds->oswLength(), 
-        summaryTds->errLength(), summaryTds->diagLength(), 
+        summaryTds->errorLength(), summaryTds->diagnosticLength(), 
         summaryTds->aemLength());
     return sc;
 }
