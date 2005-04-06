2b678da20539f6732d0b1d54c4b40b49094b751f
heather <>
2005-04-06 20:07:43 +0000

    default tree size 25 GB

--- src/RootIoSvc.cxx
+++ src/RootIoSvc.cxx
@@ -2,7 +2,7 @@
 * @file RootIoSvc.cxx
 * @brief definition of the class RootIoSvc
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/RootIoSvc.cxx,v 1.14 2005/04/06 06:44:04 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/RootIoSvc.cxx,v 1.15 2005/04/06 16:16:39 heather Exp $
 *  Original author: Heather Kelly heather@lheapop.gsfc.nasa.gov
 */
 
@@ -28,7 +28,7 @@
 * \brief Service that implements the IRunable interface, to control the event loop.
 * \author Heather Kelly heather@lheapop.gsfc.nasa.gov
 * 
-* $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/RootIoSvc.cxx,v 1.14 2005/04/06 06:44:04 heather Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/RootIoSvc.cxx,v 1.15 2005/04/06 16:16:39 heather Exp $
 */
 
 // includes
@@ -194,8 +194,8 @@ StatusCode RootIoSvc::initialize ()
         Long64_t maxTreeSize = m_treeSize * 1000000;
         TTree::SetMaxTreeSize(maxTreeSize);
     } else if (m_treeSize == 0) {
-        // 10 GB default
-        Long64_t maxTreeSize = 10000000000;
+        // 25 GB default
+        Long64_t maxTreeSize = 25000000000;
         TTree::SetMaxTreeSize(maxTreeSize);
     }
 
