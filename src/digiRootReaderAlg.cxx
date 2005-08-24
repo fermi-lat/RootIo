30a55d3cf92ff608f8faa885a727de80b5000903
heather <>
2005-08-24 22:47:14 +0000

    handle ACD header parity

--- src/digiRootReaderAlg.cxx
+++ src/digiRootReaderAlg.cxx
@@ -45,7 +45,7 @@
  * the data in the TDS.
  *
  * @author Heather Kelly
- * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/digiRootReaderAlg.cxx,v 1.59 2005/05/23 20:35:47 heather Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/digiRootReaderAlg.cxx,v 1.60 2005/07/08 06:46:26 heather Exp $
  */
 
 class digiRootReaderAlg : public Algorithm
@@ -571,11 +571,16 @@ StatusCode digiRootReaderAlg::readAcdDigi() {
         Event::AcdDigi::Range range[2];
         range[0] = (acdDigiRoot->getRange(AcdDigi::A) == AcdDigi::LOW) ? Event::AcdDigi::LOW : Event::AcdDigi::HIGH;
         range[1] = (acdDigiRoot->getRange(AcdDigi::B) == AcdDigi::LOW) ? Event::AcdDigi::LOW : Event::AcdDigi::HIGH;
-        Event::AcdDigi::ParityError err[2];
+
+        Event::AcdDigi::ParityError err[4];
         err[0] = (acdDigiRoot->getParityError(AcdDigi::A) == AcdDigi::NOERROR) ? Event::AcdDigi::NOERROR : Event::AcdDigi::ERROR;
         err[1] = (acdDigiRoot->getParityError(AcdDigi::B) == AcdDigi::NOERROR) ? Event::AcdDigi::NOERROR : Event::AcdDigi::ERROR;
-        acdDigiTds->initLdfParameters(acdDigiRoot->getTileName(), acdDigiRoot->getTileNumber(),
-                    range, err);
+        err[2] = (acdDigiRoot->getHeaderParityError(AcdDigi::A) == AcdDigi::NOERROR) ? Event::AcdDigi::NOERROR : Event::AcdDigi::ERROR;
+        err[3] = (acdDigiRoot->getHeaderParityError(AcdDigi::B) == AcdDigi::NOERROR) ? Event::AcdDigi::NOERROR : Event::AcdDigi::ERROR;`
+
+
+        acdDigiTds->initLdfParameters(acdDigiRoot->getTileName(), 
+                        acdDigiRoot->getTileNumber(), range, err);
         acdDigiTdsCol->push_back(acdDigiTds);
     }
 
