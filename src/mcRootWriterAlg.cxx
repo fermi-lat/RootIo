a74c9a7869f8ee8b5e7c7d0b007ef9bf3b53016a
heather <>
2005-09-12 08:01:31 +0000

    add exception handling for ROOT write errors

--- src/mcRootWriterAlg.cxx
+++ src/mcRootWriterAlg.cxx
@@ -39,7 +39,7 @@
  * @brief Writes Monte Carlo TDS data to a persistent ROOT file.
  *
  * @author Heather Kelly
- * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/mcRootWriterAlg.cxx,v 1.41 2005/07/13 12:58:59 chamont Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/mcRootWriterAlg.cxx,v 1.42 2005/08/13 05:52:41 heather Exp $
  */
 
 class mcRootWriterAlg : public Algorithm
@@ -516,15 +516,22 @@ void mcRootWriterAlg::writeEvent()
     //    tree.  The m_mcEvt object is cleared for the next event.
 
     static int eventCounter = 0;
+try {
     TDirectory *saveDir = gDirectory;
     m_mcTree->GetCurrentFile()->cd();
-    //m_mcFile->cd();
     m_mcTree->Fill();
-    //m_mcEvt->Clear();
     ++eventCounter;
     if (m_rootIoSvc)
-        if (eventCounter % m_rootIoSvc->getAutoSaveInterval()== 0) m_mcTree->AutoSave();
+        if (eventCounter % m_rootIoSvc->getAutoSaveInterval()== 0) 
+            m_mcTree->AutoSave();
     saveDir->cd();
+ } catch(...) { 
+    std::cerr << "Failed to write the event to file" << std::endl; 
+    std::cerr << "Exiting..." << std::endl; 
+    std::cerr.flush(); 
+    exit(1); 
+ } 
+
     return;
 }
 
@@ -537,14 +544,21 @@ void mcRootWriterAlg::close()
     //    is filled.  Writing would create 2 copies of the same tree to be
     //    stored in the ROOT file, if we did not specify kOverwrite.
 
+try {
     TDirectory *saveDir = gDirectory;
     TFile *f = m_mcTree->GetCurrentFile();
     f->cd();
-    //m_mcFile->cd();
     m_mcTree->BuildIndex("m_runId", "m_eventId");
     f->Write(0, TObject::kWriteDelete);
     f->Close();
     saveDir->cd();
+ } catch(...) { 
+    std::cerr << "Failed to final write to MC file" << std::endl; 
+    std::cerr << "Exiting..." << std::endl; 
+    std::cerr.flush(); 
+    exit(1); 
+ } 
+
     return;
 }
 
