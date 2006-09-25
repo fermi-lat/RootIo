72169209a5d8765053d1190da30006c343f2cb04
wilko <>
2006-09-25 20:05:01 +0000

    Change open root files to RootReaderUtil::addFilesToChain in order to work with xrootd.

--- src/mcRootReaderAlg.cxx
+++ src/mcRootReaderAlg.cxx
@@ -32,6 +32,7 @@
 #include "facilities/Util.h"
 #include "commonData.h"
 #include "RootIo/IRootIoSvc.h"
+#include "RootConvert/Utilities/RootReaderUtil.h"
 
 // ADDED FOR THE FILE HEADERS DEMO
 #include "RootIo/FhTool.h"
@@ -49,8 +50,9 @@
  * the data in the TDS.
  *
  * @author Heather Kelly
- * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/mcRootReaderAlg.cxx,v 1.57 2006/07/19 18:16:26 heather Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/mcRootReaderAlg.cxx,v 1.58 2006/08/22 16:23:57 heather Exp $
  */
+
 
 class mcRootReaderAlg : public Algorithm
 {	
@@ -185,36 +187,14 @@ StatusCode mcRootReaderAlg::initialize()
     TDirectory *saveDir = gDirectory;	
     m_mcTree = new TChain(m_treeName.c_str());
 
-    std::string emptyStr("");
-    if (m_fileName.compare(emptyStr) != 0) {
-      TFile f(m_fileName.c_str());
-      if (!f.IsOpen()) {
-        log << MSG::ERROR << "ROOT file " << m_fileName.c_str() 
-            << " could not be opened for reading." << endreq;
-        return StatusCode::FAILURE;
-      }
-      f.Close();
-      m_mcTree->Add(m_fileName.c_str());
-      log << MSG::INFO << "Opened file: " << m_fileName.c_str() << endreq;
-    } else {
-      const std::vector<std::string> fileList = m_fileList.value( );
-      std::vector<std::string>::const_iterator it;
-      std::vector<std::string>::const_iterator itend = fileList.end( );
-      for (it = fileList.begin(); it != itend; it++) {
-          std::string theFile = (*it);
-          TFile f(theFile.c_str());
-          if (!f.IsOpen()) {
-              log << MSG::ERROR << "ROOT file " << theFile.c_str() 
-                  << " could not be opened for reading." << endreq;
-              return StatusCode::FAILURE;
-           }
-           f.Close();
-           m_mcTree->Add(theFile.c_str());
-           log << MSG::INFO << "Opened file: " << theFile.c_str() << endreq;
-      }
+
+    // add root files to TChain and check if files exist
+    StatusCode openSc= RootPersistence::addFilesToChain(m_mcTree, m_fileName, m_fileList, log);
+    if (openSc.isFailure()) {
+      return  openSc;
     }
     
-
+    
     m_mcEvt = 0;
     m_mcTree->SetBranchAddress("McEvent", &m_mcEvt);
     m_common.m_mcEvt = m_mcEvt;
