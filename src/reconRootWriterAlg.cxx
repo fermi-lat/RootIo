6ea014bbd17c3eb2356f63d49ca22706b6691ffb
heather <>
2006-12-03 13:46:30 +0000

    use TClonesArray for GcrXtal

--- src/reconRootWriterAlg.cxx
+++ src/reconRootWriterAlg.cxx
@@ -59,7 +59,7 @@
 * @brief Writes Recon TDS data to a persistent ROOT file.
 *
 * @author Heather Kelly and Tracy Usher
-* $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/reconRootWriterAlg.cxx,v 1.76 2006/07/17 21:33:01 heather Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/RootIo/src/reconRootWriterAlg.cxx,v 1.77 2006/11/13 10:38:56 claval Exp $
 */
 
 class reconRootWriterAlg : public Algorithm
@@ -684,17 +684,13 @@ void reconRootWriterAlg::fillGcrXtal(CalRecon *calRec, Event::GcrXtalCol* gcrXta
     
     for (gcrXtalColIterTds = gcrXtalColTds->begin(); gcrXtalColIterTds != gcrXtalColTds->end(); gcrXtalColIterTds++) {
 	
-	GcrXtal* gcrXtalRoot = new GcrXtal();
-	RootPersistence::convert(**gcrXtalColIterTds,*gcrXtalRoot) ; 
+	//GcrXtal* gcrXtalRoot = new GcrXtal();
+    GcrXtal* gcrXtalRoot = calRec->addGcrXtal();
+    RootPersistence::convert(**gcrXtalColIterTds,*gcrXtalRoot) ; 
 	
-   /** log << MSG::INFO << "gcrXtalRoot->getXtal()->getPackedId()= " << gcrXtalRoot->getXtal()->getPackedId().getTower()
-    << "/" << gcrXtalRoot->getXtal()->getPackedId().getLayer() << "/" << gcrXtalRoot->getXtal()->getPackedId().getColumn()<< endreq;   
-   */	
-	
-        calRec->addGcrXtal(gcrXtalRoot) ;   
+       // calRec->addGcrXtal(gcrXtalRoot) ;   
     }   
     
-        //log << MSG::INFO << "reconRootWriterAlg::fillGcrXtal END" << endreq;   
 
     
     return;   
