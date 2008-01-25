e0b36cfa9685cc1a240f84e59fd18370ce9b61e0
chamont <>
2008-01-25 23:24:24 +0000

    *** empty log message ***

--- src/test/test_RootIo.sh
+++ src/test/test_RootIo.sh
@@ -17,7 +17,7 @@ make test || eval 'echo VALIDATION BUILD FAILED ; exit' ;
 cd ../../../Gleam/*/cmt
 . setup.sh
 cd ../../../rootTestData/*/data/vertical_surface_muons
-../../../../Gleam/*/$CMTCONFIG/Gleam.exe ./jobOptions.txt
+#../../../../Gleam/*/$CMTCONFIG/Gleam.exe ./jobOptions.txt | tee jobOptions.log 2>&1
 
 
 #=================================================
@@ -28,7 +28,7 @@ cd ../../../../RootIo/*/cmt
 . setup.sh
 cd ../src/test
 rm -f jobOptions.log
-../../$CMTCONFIG/test_RootIo.exe jobOptions.txt | grep -E '(Deposited Energy|VolumeID|TDS)' > jobOptions.log
+../../$CMTCONFIG/test_RootIo.exe jobOptions.txt | tee jobOptions.fulllog | grep -E '(Deposited Energy|VolumeID|TDS)' > jobOptions.log 2>&1
 diff -q -s jobOptions.log jobOptions.ref || eval 'echo VALIDATION DIFF FAILED ; exit' ;
 
 
