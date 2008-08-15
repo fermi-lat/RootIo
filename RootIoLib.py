73e46e1ec63e83eb8eafab53881f3f15083d28c9
ecephas <>
2008-08-15 21:42:36 +0000

    Added Lib file (e.g., <package>Lib.py) for each package that creates a library.

---
+++ RootIoLib.py
@@ -0,0 +1,9 @@
+#$Header$
+def generate(env, **kw):
+    if not kw.get('depsOnly', 0):
+        env.Tool('addLibrary', library = ['RootIo'])	
+    env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('addLibrary', library = env['rootGuiLibs'])
+
+def exists(env):
+    return 1;
